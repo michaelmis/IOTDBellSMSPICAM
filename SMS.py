afec137a92a80013516dec85446d7d61efda3348
michaelmis <michaelmis@hotmail.com>
2018-04-30 15:36:18 +0800

    Update SMS.py

--- SMS.py
+++ SMS.py
@@ -3,6 +3,6 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login("youremail@gmail.com", "yourpassword")
 msg = "Message from the doorbell at home. Read email I have sent a photo!"
-server.sendmail("youremail@gmail.com", "0404XXXXX5@transmitsms.com", msg)
-#Burstsms uses the address @transmitsms.com for communication to the "email to SMS" service.
+server.sendmail("youremail@gmail.com", "0404XXXXX8@transmitsms.com", msg)
+#Your SMS provider. I use a company http://www.burstsms.com.au/. Burstsms uses the address @transmitsms.com
 server.quit()
